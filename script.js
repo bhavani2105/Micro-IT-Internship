cde09d26a08df5d125669a261ae3240f39beb896
bhavani2105 <bhavaniponnada2105@gmail.com>
2025-04-29 10:15:58 +0530

    this is my portfolio project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple form submit handler
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for contacting me!");
+  });
